d993fe17ec98844c647204dac7e3d9cbd41712cb
Miguel Marzo <miguel-marzo@hotmail.es>
2020-02-19 13:11:34 +0100

    Agrega firebase y comando de despliegue

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB5k2SNBtgJ7uBwen8cJf-M99t2W28P7gs",
+    authDomain: "tableplay-574b3.firebaseapp.com",
+    databaseURL: "https://tableplay-574b3.firebaseio.com",
+    projectId: "tableplay-574b3",
+    storageBucket: "tableplay-574b3.appspot.com",
+    messagingSenderId: "200679471384",
+    appId: "1:200679471384:web:37f2603b5c5f346994866d"
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./views/App.tsx";
+import { FirebaseContext, Firebase } from './firebase'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const rootNode = document.querySelector('#root')
+
+ReactDOM.render(
+    <FirebaseContext.Provider value={new Firebase()}>
+      <App />
+    </FirebaseContext.Provider>,
+    rootNode
+  )
